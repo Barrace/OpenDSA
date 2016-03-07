8394ffa62204c3e40cccd1c9c2ae60461f0e2a2b
yangy01 <yangy01@uwosh.edu>
2016-03-06 23:41:05 -0600

    yangy01Avinline

--- AV/Development/YuphengKnaps/YuphengKnapsack1.js
+++ AV/Development/YuphengKnaps/YuphengKnapsack1.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 
     JSAV.init();
-
+    var arr;
 
     var av = new JSAV("YuphengKnapsack1");
     //var theArray = av.ds.matrix({rows: 5, columns: 6, style: "table"});
@@ -16,32 +16,57 @@ $(document).ready(function () {
         v = ["V", 1, 2, 3, 4, 5];
     //a[1] = 0;
     //alert(a.size());
-    for(var ji = 1; ji < a.length; ji++)
+    //arr = av.ds.matrix([v, a, b, c, d]);
+    /*for(var ji = 1; ji < a.length; ji++)
     {
+        //var arr = av.ds.matrix([v, a, b, c, d]);
         a[ji] = 6;
         av.step();
-    }
-    var arr = av.ds.matrix([v, a, b,
-        c, d]);
+    }*/
+    //av.step();
+    //av.ds.matrix([v, a, b, c, d]); 
     //arr..highlight([v,a,b,c,d])
     //arr.hide();
     //a[2] = 0;
     //arr.show();
     
 
-    //var arr = av.ds.matrix({rows: 5, columns: 6, style: "table"});
+    arr = av.ds.matrix({rows: 5, columns: 6, style: "table"});
+    /*for(var r = 0; r < arr.rows; r++)
+    {
+        for(var c = 0; c < arr.columns; c++)
+        {
+            arr.value(0,0,"V");
+            arr.value(0,c,c);
+            arr.value(r,0,r);
+        }
+        av.step();
+    }*/
+    arr.value(0,0,"V");
+    arr.value(0,1,1);
+    arr.value(0,2,2);
+    arr.value(0,3,3);
+    arr.value(0,4,4);
+    arr.value(0,5,5);
+    
+    arr.value(1,0,0);
+    arr.value(2,0,1);
+    arr.value(3,0,2);
+    arr.value(4,0,3);
     //alert(arr.rows());
     //alert(arr.columns());
     //var arr = av.ds.matrix([[],[],[],[],[],[]]);
-
-    /*for(var i = 0; i < 6; i++)
+    //var count = 0;
+    /*for(var i = 1; i < arr.rows; i++)
     {
-        for(var j = 0; j < 7; j++)
+        for(var j = 1; j < arr.columns; j++)
         {
-            arr..highlight([v,a,b,c,d])
+           arr.value(4,4,count);
         }
-        
-        av.step();
+        count++;
+        arr.step();
+        //arr.show(); 
+
         
 
     }*/
@@ -51,13 +76,15 @@ $(document).ready(function () {
     // All that it will do is affect what you get to see on the
     // initial slide.
     av.displayInit();
+
     // We are now starting a new slide (#2)
-    av.umsg("... and text after displayInit()", {preserve: false});
+    av.umsg("... and text after displayInit()", {preserve: true});
     //arr.swap(3,7);
     //var arr = av.ds.matrix([v, a, b,
         //c, d]);
-    //av.step();
-    /*for(var i = 0; i < arr.rows; i++)
+    /*var count = 0;
+    var j = 1;
+    for(var i = 1; i < arr.rows; i++)
     {
         //for(var j = 0; j < arr.columns(); j++)
         //{
@@ -65,20 +92,82 @@ $(document).ready(function () {
             
             //a.highlight(i+1);
             
-            //arr.value(a[i],0);
+            arr.value(i,j,count);
             //arr.value(a[i],0);
 
             //arr = av.ds.matrix([v,a,b,c,d]);
             //a.unhighlight(i);
             //av.recorded();
-            av.step();
+            //av.step();
         //}
-        //av.step();
+        av.step();
+        j++;
     }*/
     //a.value(1,0);
     //how();
-    arr.show();
+    //arr.show();
+    //av.step();
+    /*var bool = true;
+    var i = 1,
+        j = 1,
+        count = 0;
+    while(i != arr.rows)
+    {
+        while(j != arr.columns)
+        {
+            arr.value(i,j,count);
+            j++;
+        }
+        i++;
+        av.step();
+    }*/
+    arr.value(1,1,0);
     av.step();
+    arr.value(1,2,0);
+    av.step();
+    arr.value(1,3,0);
+    av.step();
+    arr.value(1,4,0);
+    av.step();
+    arr.value(1,5,0);
+    av.step();
+
+    arr.value(2,1,0);
+    av.step();
+    arr.value(2,2,0);
+    av.step();
+    arr.value(2,3,5);
+    av.step();
+    arr.value(2,4,5);
+    av.step();
+    arr.value(2,5,5);
+    av.step();
+
+
+    arr.value(3,1,0);
+    av.step();
+    arr.value(3,2,3);
+    av.step();
+    arr.value(3,3,5);
+    av.step();
+    arr.value(3,4,5);
+    av.step();
+    arr.value(3,5,8);
+    av.step();
+
+
+    arr.value(4,1,4);
+    av.step();
+    arr.value(4,2,4);
+    av.step();
+    arr.value(4,3,7);
+    av.step();
+    arr.value(4,4,9);
+    av.step();
+    arr.value(4,5,9);
+    av.step();
+
+    // array[funcname].apply(array, [].slice.call(arguments, 1));
     // We are now starting a new slide (#3)
     av.umsg("Text after av.step()");
     av.recorded();
@@ -89,5 +178,6 @@ $(document).ready(function () {
     // So, unless you are trying to add slides on-the-fly
     // interactively, you don't want to do this.
     // av.umsg("Text after av.recorded()");
+
 
 });
